29cde5567fb4649fcd30d5268061d2b4185e70b9
Ivan <mimeev91@gmail.com>
2022-10-24 23:25:41 +0300

    v4

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -19,13 +19,13 @@ public class UserDaoImpl implements UserDao {
     public List<User> allUser() {
         return entityManager.createQuery("Select a from User a ", User.class).getResultList();
     }
-    @Transactional
+
     @Override
     public void add(User user) {
         entityManager.persist(user);
     }
 
-    @Transactional
+
     @Override
     public void edit(User user) {
         entityManager.merge(user);
@@ -40,7 +40,7 @@ public class UserDaoImpl implements UserDao {
         }
         return user;
     }
-    @Transactional
+
     @Override
     public void deleteById(long id) {
         int isSuccessful = entityManager.createQuery("delete from User p where p.id=:id")
