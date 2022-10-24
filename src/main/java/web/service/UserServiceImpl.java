bf27d12e773701ce4768500fa673baa52a8fd7b8
Ivan <mimeev91@gmail.com>
2022-10-25 02:39:23 +0300

    v4

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package web.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
 import web.model.User;
 
